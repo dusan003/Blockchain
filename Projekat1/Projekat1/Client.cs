9b8db8498d8162ebf0951af14333511b9fe33ca8
dusan003 <148562355+dusan003@users.noreply.github.com>
2023-12-08 23:17:38 +0100

    Add files via upload

---
+++ Projekat1/Projekat1/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat1
+{
+    class Client : IClient
+    {
+        public void SendData(string data)
+        {
+
+        }
+    }
+}
