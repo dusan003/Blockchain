365b6ad52fa74d2d42a7308886ade7f3bae76c91
dusan003 <148562355+dusan003@users.noreply.github.com>
2024-01-02 16:20:26 +0100

    Add files via upload

---
+++ Projekat1/Common/Blockchain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Blockchain
+    {
+        public List<Block> Blocks { get; private set; }
+
+        public Blockchain()
+        {
+            Blocks = new List<Block>();
+        }
+
+        public void AddToChain(Block block)
+        {
+            // Set the block id and add it to the blockchain
+            block.Id = Blocks.Count + 1;
+            Blocks.Add(block);
+        }
+    }
+}
---
+++ Projekat1/SmartContract/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartContract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start server for client
+            using (ServiceHost host = new ServiceHost(typeof(SmartContractClient)))
+            {
+                string address = "net.tcp://localhost:4000/ISmartContractClient";
+                NetTcpBinding binding = new NetTcpBinding();
+
+                host.AddServiceEndpoint(typeof(ISmartContractClient), binding, address);
+
+                host.Open();
+                Console.WriteLine($"Servis je uspesno pokrenut na adresi : {address}");
+
+                // Start server for miner
+                using (ServiceHost hostMiner = new ServiceHost(typeof(SmartContractMiner)))
+                {
+                    string addressMiner = "net.tcp://localhost:4000/ISmartContractMiner";
+                    NetTcpBinding bindingMiner = new NetTcpBinding();
+
+                    hostMiner.AddServiceEndpoint(typeof(ISmartContractMiner), bindingMiner, addressMiner);
+
+                    hostMiner.Open();
+                    Console.WriteLine($"Servis je uspesno pokrenut na adresi : {addressMiner}");
+                    Console.ReadLine();
+                    hostMiner.Close();
+                }
+
+                host.Close();
+
+            }
+
+
+        }
+    }
+}
